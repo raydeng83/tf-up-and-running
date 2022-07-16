62936d1453dd5652f41ece5465d9791a51acfdbf
raydeng83 <ray.deng83@gmail.com>
2022-07-16 09:50:57 -0400

    stage working

--- ch4/live/stage/data-stores/mysql/main.tf
+++ ch4/live/stage/data-stores/mysql/main.tf
@@ -13,7 +13,7 @@ terraform {
     # manually, uncomment and fill in the config below.
 
     bucket         = "tf-ldeng-ch4-bucket-1"
-    key            = "stage/data-stores/mysql/terraform.tfstate"
+    key            = "state/terraform.tfstate"
     region         = "us-east-2"
     dynamodb_table = "tf-ldeng-ch4-table-1"
     encrypt        = true
--- ch4/live/stage/services/webserver-cluster/main.tf
+++ ch4/live/stage/services/webserver-cluster/main.tf
@@ -3,11 +3,12 @@ provider "aws" {
 }
 
 module "webserver_cluster" {
-  source = "../../../modules/services/webserver-cluster"
+  # source = "../../../modules/services/webserver-cluster"
+  source = "github.com/raydeng83/tf-modules//services/webserver-cluster?ref=v0.0.6"
 
   cluster_name           = "webservers-stage"
   db_remote_state_bucket = "tf-ldeng-ch4-bucket-1"
-  db_remote_state_key    = "stage/data-stores/mysql/terraform.tfstate"
+  db_remote_state_key    = "state/terraform.tfstate"
 
   instance_type = "t2.micro"
   min_size      = 2
